919e7e389a9175d6d24c68c298a50b0048d507bf
Siddhant Sehgal <sehgal.siddhant999@gmail.com>
2021-07-08 21:23:33 +0530

    reverted a commit

--- src/register/registerCsvAsTable.ts
+++ src/register/registerCsvAsTable.ts
@@ -1,31 +1,9 @@
 import joplin from 'api';
 import { MenuItemLocation } from 'api/types';
-import csvAsTable from '../../utils/csvAsTable/csvAsTable';
+import csvAsTable from '../utils/csvAsTable/csvAsTable';
 
 export default async function registerCsvAsTable(): Promise<void> {
-
-	// Import CSV Dialog Configuration
-	const dialogs = joplin.views.dialogs;
-	const importCsvDialog = await dialogs.create('importCsvDialog');
-	await dialogs.setHtml(importCsvDialog, `
-		<form name="hiddenForm" id="hiddenForm">
-			<input style={ border: '2px solid lightblue', borderRadius: '5px' } type="hidden" name="hiddenInput" id="hiddenInput">
-		</form>
-		<label>Select CSV File: </label>
-		<input id="importCsvAsTable" onChange="importCsv(this)" type="file" accept=".csv" />
-	`);
-	await dialogs.setButtons(importCsvDialog, [
-		{
-			id: 'ok',
-		},
-		{
-			id: 'cancel',
-		},
-	]);
-	await dialogs.addScript(importCsvDialog, './register/csvAsTable/importCsv.js');
-	
-
-    // Respective command for button
+    // Respective command for main button
 		await joplin.commands.register({
             name: 'pasteCsvAsTable',
             label: 'CSV as Table',
@@ -43,8 +21,7 @@ export default async function registerCsvAsTable(): Promise<void> {
             name: 'importCsvAsTable',
             label: 'CSV as Table',
             execute: async () => {
-				let data = await dialogs.open(importCsvDialog);
-				const csv = data.formData.hiddenForm.hiddenInput;
+				const csv =`CONTENT TYPE,TITLE,ABBR,ISSN,e-ISSN\nJournals,ACM Computing Surveys ,ACM Comput. Surv.,0360-0300,1557-7341\nJournals,ACM Journal of Computer Documentation ,ACM J. Comput. Doc.,1527-6805,1557-9441\nJournals,ACM Journal on Emerging Technologies in Computing Systems ,J. Emerg. Technol. Comput. Syst.,1550-4832,1550-4840\nJournals,Journal of Data and Information Quality ,J. Data and Information Quality,1936-1955,1936-1963`;
 				const importCsvAsTable = await csvAsTable(csv);
 				await joplin.commands.execute("insertText", importCsvAsTable);
 				await joplin.commands.execute('editor.focus');
